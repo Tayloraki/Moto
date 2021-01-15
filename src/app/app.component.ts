4ec8d5628bc3d6dd0706d6d421cf1321e65056b2
Taylor Akimoto <teakimot@usc.edu>
2021-01-14 16:53:02 -0800

    Initial Link Table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  links: string[] = ['link1', 'link2', 'link3']
 
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 }
