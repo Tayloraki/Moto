b96e36012089f85fe4f3652766e2efe97661aebb
palaklal <palaklal18@gmail.com>
2021-01-25 17:23:33 -0800

    created an endpoint to return recipe information using the recipe-scraper package

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
+const recipeScraper = require("recipe-scraper")
 
 app.listen(8000, () => {
   console.log('Server started!')
 })
 
-app.get('/', (req, res) => {
+app.get('/', (req: any, res: any) => {
     res.send({ hello: 'world' })
+})
+
+app.get('/recipe/*', (req: any, res: any) => {
+  let recipeUrl = req.params[0]
+  recipeScraper(recipeUrl).then(recipe => {
+    res.send({ value: recipe })
+  }).catch(error => {
+    res.send({ error: error })
+  })
 })
