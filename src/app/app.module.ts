ebf95d5e8539bb0ba51a33a79f0e4c8135385288
Taylor Akimoto <teakimot@usc.edu>
2021-04-29 16:10:19 -0700

    installed Firebase and Firebase UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
+// Firebase
+import { AngularFireModule } from '@angular/fire'
+import { AngularFirestoreModule } from '@angular/fire/firestore'
+import { AngularFireAuthModule } from '@angular/fire/auth'
+import { environment } from '../environments/environment'
+
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component'
 import { RecipesSummaryComponent } from './recipes-summary/recipes-summary.component'
-import { RecipeDetailsModalComponent } from './recipe-details-modal/recipe-details-modal.component';
+import { RecipeDetailsModalComponent } from './recipe-details-modal/recipe-details-modal.component'
 import { IngredientSearchComponent } from './ingredient-search/ingredient-search.component'
 
 @NgModule({
@@ -25,6 +31,9 @@ import { IngredientSearchComponent } from './ingredient-search/ingredient-search
     FormsModule,
     HttpClientModule,
     NgbModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig, 'moto'),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
   ],
   providers: [],
   bootstrap: [AppComponent],
