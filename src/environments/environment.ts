73178cd54e731638de32aefa7fb1d2098d79c514
Taylor Akimoto <teakimot@usc.edu>
2021-05-03 19:18:06 -0700

    Re-added environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,31 @@
+// import { config } from "rxjs";
+import { config } from '../config/config'
+
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  nutritionixAppId: config.nutritionixAppId,
+  nutritionixAppKey: config.nutritionixAppKey,
+  nutritionixRemoteUserId: config.nutritionixRemoteUserId,
+  firebaseConfig: {
+    apiKey: config.firebase_apiKey,
+    authDomain: config.firebase_authDomain,
+    projectId: config.firebase_projectId,
+    storageBucket: config.firebase_storageBucket,
+    messagingSenderId: config.firebase_messagingSenderId,
+    // appId: config.firebase_appId,
+    // measurementId: config.firebase_measurementId
+  },
+}
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
